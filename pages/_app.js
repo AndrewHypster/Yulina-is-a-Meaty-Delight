d0ed8fbe6759e89b1d9c401cd885d8bf9dd52f63
AndrewHypster <andrewgrechukh123@gmail.com>
2024-08-23 14:03:59 +0300

    redux user state

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import { store } from "@/redux_toolkit/store";
 import "@/styles/globals.css";
 import "@/styles/slider.css";
+import { Providers } from "../redux_toolkit/provider";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers store={store}>
+      <Component {...pageProps} />
+    </Providers>
+  );
 }
