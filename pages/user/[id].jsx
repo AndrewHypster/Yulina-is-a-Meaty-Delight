c3555e2050383b43e180ebe609ddbb21d9469589
AndrewHypster <andrewgrechukh123@gmail.com>
2024-09-30 16:49:29 +0300

    useRouter

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -3,10 +3,11 @@ import Header from "@/components/header";
 import MyHead from "@/components/myHead";
 import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function User() {
-  const router = wiouter();
+  const router = useRouter();
   const [userInfo, setUserInfo] = useState({});
 
   useEffect(() => {
