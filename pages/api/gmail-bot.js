168c1f403b891b640d0386b6e804389497f4a0d6
AndrewHypster <andrewgrechukh123@gmail.com>
2024-10-04 11:08:39 +0300

    google app pass

--- pages/api/gmail-bot.js
+++ pages/api/gmail-bot.js
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: 'process.env.GMAIL_USER', // ваш Gmail акаунт
-        pass: 'process.env.GMAIL_PASS', // спеціальний "app password" з налаштувань безпеки
+        user: process.env.GMAIL_USER, // ваш Gmail акаунт
+        pass: process.env.GMAIL_PASS, // спеціальний "app password" з налаштувань безпеки
       },
     });
 
